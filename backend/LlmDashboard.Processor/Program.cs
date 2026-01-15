318161f8067ced001efc33464eeb586b4fee6802
wkud <vopox07@gmail.com>
2026-01-15 16:26:15 +0100

    Add MassTransit and Serilog to Processor

--- backend/LlmDashboard.Processor/Program.cs
+++ backend/LlmDashboard.Processor/Program.cs
@@ -1,7 +1,48 @@
 using LlmDashboard.Processor;
+using MassTransit;
+using Serilog;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+builder.Services.AddMassTransit(x =>
+{
+    x.SetKebabCaseEndpointNameFormatter();
+
+    x.AddConsumers(typeof(Program).Assembly);
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        var rabbitMqHost = builder.Configuration["RabbitMQ:Host"] ?? "localhost";
+        var rabbitMqUsername = builder.Configuration["RabbitMQ:Username"] ?? "guest";
+        var rabbitMqPassword = builder.Configuration["RabbitMQ:Password"] ?? "guest";
+
+        cfg.Host(rabbitMqHost, h =>
+        {
+            h.Username(rabbitMqUsername);
+            h.Password(rabbitMqPassword);
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
-host.Run();
+
+try
+{
+    Log.Information("Starting processor application");
+    host.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
